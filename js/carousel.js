c7d11e95707bd30302f08396a4e87b17cf8a06d8
LUCAS PEREIRA DOMINGUEZ <pereiraluccas68@gmail.com>
2025-08-11 11:42:37 -0300

    Inserindo imagens e estilizando

--- js/carousel.js
+++ js/carousel.js
@@ -9,12 +9,51 @@ let carouselArr = [];
 //class Carousel
 class Carousel {
 
-    
+    constructor(image, title, url) {
+        this.image = image
+        this.title = title
+        this.url = url
+    }
+
+    static CreateTags() {
+        return {
+            imgContainer: document.createElement('div'),
+            imgLink: document.createElement('a'),
+            img: document.createElement('img'),
+            text: document.createElement('p')
+        }
+    }
+
+    static InsertImg(arr) {
+        if (this.image == null && typeof arr !== "object" && arr.length == 0)
+            throw "Imagem inválida."
+        
+        const container = document.getElementById('carousel')
+
+        let images = []
+
+        // Criando elementos
+        for (let i in arr) {
+            const newDom = Carousel.CreateTags()
+            newDom.img.src = arr[i].image
+            newDom.img.title = arr[i].title || ''
+            newDom.imgLink.href = arr[i].url || '#'
+            newDom.text.textContent = arr[i].title || ''
+            // Append
+            newDom.imgLink.appendChild(newDom.img)
+            newDom.imgContainer.appendChild(newDom.imgLink)
+            newDom.imgContainer.appendChild(newDom.text)
+            container.appendChild(newDom.imgContainer)
+            images.push(newDom.imgContainer)
+        }
+        return images
+    }
       
     static Start(arr){
         if(arr){
-
+            console.log(arr)
             if(arr.length > 0){
+                const images = this.InsertImg(arr)
                 Carousel._sequence = 0;
                 Carousel._size = arr.length;
                 Carousel.Next(); //start
