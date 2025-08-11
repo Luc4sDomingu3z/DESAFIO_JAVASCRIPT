f145c12b8b4b035c926f06e02f18c2e8a6f51af2
Luc4sDomingu3z <pereiraluccas68@gmail.com>
2025-08-11 18:14:05 -0300

    att

--- js/carousel.js
+++ js/carousel.js
@@ -1,71 +1,73 @@
-
-
 //carousel
 
 //Array storage class
 let carouselArr = [];
 
-
 //class Carousel
 class Carousel {
+  constructor(image, title, url) {
+    this.image = image;
+    this.title = title;
+    this.url = url;
+  }
 
-    constructor(image, title, url) {
-        this.image = image
-        this.title = title
-        this.url = url
+  static CreateTags() {
+    return {
+      imgContainer: document.createElement("div"),
+      imgLink: document.createElement("a"),
+      img: document.createElement("img"),
+      text: document.createElement("p"),
+    };
+  }
+
+  static InsertImg(arr) {
+    if (this.image == null && typeof arr !== "object" && arr.length == 0)
+      throw "Imagem inválida.";
+
+    const container = document.getElementById("carousel");
+
+    let images = [];
+
+    // Criando elementos
+    for (let i in arr) {
+      const newDom = Carousel.CreateTags();
+      newDom.img.src = arr[i].image;
+      newDom.img.title = arr[i].title || "";
+      newDom.imgLink.href = arr[i].url || "#";
+      newDom.text.textContent = arr[i].title || "";
+      // Append
+      newDom.imgLink.appendChild(newDom.img);
+      newDom.imgContainer.appendChild(newDom.imgLink);
+      newDom.imgContainer.appendChild(newDom.text);
+      container.appendChild(newDom.imgContainer);
+      images.push(newDom.imgContainer);
+    }
+    return images;
+  }
+
+  static Start(arr) {
+    if (arr) {
+      console.log(arr);
+      if (arr.length > 0) {
+        const images = this.InsertImg(arr);
+        Carousel._sequence = 0;
+        Carousel._size = arr.length;
+        Carousel.Next(images); //start
+        Carousel._interval = setInterval(function () {
+          Carousel.Next(images);
+        }, 5000);
+      }
+    } else {
+      throw "Method Start need a Array Variable.";
+    }
+  }
+
+  static Next(imgs) {
+    if (Carousel._size === Carousel._sequence) {
+      Carousel._sequence = 0
     }
 
-    static CreateTags() {
-        return {
-            imgContainer: document.createElement('div'),
-            imgLink: document.createElement('a'),
-            img: document.createElement('img'),
-            text: document.createElement('p')
-        }
-    }
-
-    static InsertImg(arr) {
-        if (this.image == null && typeof arr !== "object" && arr.length == 0)
-            throw "Imagem inválida."
-        
-        const container = document.getElementById('carousel')
-
-        let images = []
-
-        // Criando elementos
-        for (let i in arr) {
-            const newDom = Carousel.CreateTags()
-            newDom.img.src = arr[i].image
-            newDom.img.title = arr[i].title || ''
-            newDom.imgLink.href = arr[i].url || '#'
-            newDom.text.textContent = arr[i].title || ''
-            // Append
-            newDom.imgLink.appendChild(newDom.img)
-            newDom.imgContainer.appendChild(newDom.imgLink)
-            newDom.imgContainer.appendChild(newDom.text)
-            container.appendChild(newDom.imgContainer)
-            images.push(newDom.imgContainer)
-        }
-        return images
-    }
-      
-    static Start(arr){
-        if(arr){
-            console.log(arr)
-            if(arr.length > 0){
-                const images = this.InsertImg(arr)
-                Carousel._sequence = 0;
-                Carousel._size = arr.length;
-                Carousel.Next(); //start
-                Carousel._interval = setInterval(function(){ Carousel.Next(); },5000);
-            }
-            
-        } else {
-            throw "Method Start need a Array Variable.";
-        }
-    }
-
-    static Next(){
-        
-    }
-};
+    imgs[Carousel._sequence].classList.add('slided')
+    Carousel._sequence += 1
+  }
+}
