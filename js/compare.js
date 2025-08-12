194bd9f563314afbc292a67009a926b22288997e
Luc4sDomingu3z <pereiraluccas68@gmail.com>
2025-08-12 17:44:23 -0300

    att

--- js/compare.js
+++ js/compare.js
@@ -1,63 +1,63 @@
-
 //car
 let carArr = [];
 
 class Car {
-   
 
-    constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image){
-     this.nome = nome
-     this.preco = preco
-     this.alturaCacamba = alturaCacamba
-     this.alturaVeiculo = alturaVeiculo
-     this.alturaSolo   = alturaSolo
-     this.capacidadeCarga = capacidadeCarga
-     this.motor=motor
-     this.potencia=potencia
-     this.volumeCacamba=volumeCacamba
-     this.roda=roda
-     this.image=image
-    }
-} 
+
+  constructor(nome, preco, alturaCacamba, alturaVeiculo, alturaSolo, capacidadeCarga, motor, potencia, volumeCacamba, roda, image) {
+    this.nome = nome
+    this.preco = preco
+    this.alturaCacamba = alturaCacamba
+    this.alturaVeiculo = alturaVeiculo
+    this.alturaSolo = alturaSolo
+    this.capacidadeCarga = capacidadeCarga
+    this.motor = motor
+    this.potencia = potencia
+    this.volumeCacamba = volumeCacamba
+    this.roda = roda
+    this.image = image
+  }
+}
 
 // search on array if exist carClass returning 1 if not return -1
 function GetCarArrPosition(arr, carClass) {
-    for(let i = 0; i < arr.length; i++){
-        if(arr[i].nome  === carClass.nome)
-            return i;
-    }
-    return -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].nome === carClass.nome)
+      return i;
+  }
+  return -1;
 }
 
 function SetCarToCompare(el, carClass) {
-   
-    if(carClass instanceof Car){       
-        const pos = GetCarArrPosition(carArr, carClass)
-        if(el.checked){
 
-        } else {
-          
-        }
-        console.log(carArr)
+  if (carClass instanceof Car) {
+    const pos = GetCarArrPosition(carArr, carClass)
+    console.log("posicao", pos)
+    if (el.checked) {
+      carArr.push(carClass)
     } else {
-        throw "You need set a Car Class";
+
     }
+    console.log(carArr)
+  } else {
+    throw "You need set a Car Class";
+  }
 }
 
 function ShowCompare() {
-    if(carArr.length < 2) {
-        alert("Precisa marcar 2 carros para apresentar a comparação");
-        return;
-    }
+  if (carArr.length < 2) {
+    alert("Precisa marcar 2 carros para apresentar a comparação");
+    return;
+  }
 
-    UpdateCompareTable();
-    document.getElementById("compare").style.display = "block";
+  UpdateCompareTable();
+  document.getElementById("compare").style.display = "block";
 }
 
-function HideCompare(){
-    document.getElementById("compare").style.display = "none"; 
+function HideCompare() {
+  document.getElementById("compare").style.display = "none";
 }
 
 function UpdateCompareTable() {
-    
+
 }
