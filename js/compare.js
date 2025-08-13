9a7c729ef2c32518a5228dea7fbbf9e5326aaa2f
Luc4sDomingu3z <pereiraluccas68@gmail.com>
2025-08-13 19:25:52 -0300

    Comecando a dedicar os dados na tabela

--- js/compare.js
+++ js/compare.js
@@ -33,11 +33,11 @@ function SetCarToCompare(el, carClass) {
   if (carClass instanceof Car) {
     const pos = GetCarArrPosition(carArr, carClass)
     if (el.checked) {
-        if (pos < 0)  carArr.push(carClass)
+      if (pos < 0) carArr.push(carClass)
 
-        if (carArr.length > 2) carArr = carArr.filter((el, i, arr) => i > arr.length - arr.length)
+      if (carArr.length > 2) carArr = carArr.filter((el, i, arr) => i > arr.length - arr.length)
     } else {
-        carArr = carArr.filter((el, i) => i != pos)
+      carArr = carArr.filter((el, i) => i != pos)
     }
   } else {
     throw "You need set a Car Class";
@@ -59,18 +59,36 @@ function HideCompare() {
 }
 
 function UpdateCompareTable(carsData) {
-    if (typeof carsData === "object" && carsData.length === 0)
-        throw "Dados vazios"
+  if (typeof carsData === "object" && carsData.length === 0)
+    throw "Dados vazios"
 
-    const table = document.querySelector('div#compare table')
-    
-    const tbody = table.querySelector('tbody')
+  const table = document.querySelector('div#compare table')
 
-    console.log(tbody)
+  const tbody = table.querySelector('tbody')
 
-    for (let i = 0; i < carsData.length; i++) {
-        for (let c = 0; c < tbody.children.length; i++) {
-            let tr = tbody.children.namedItem('tr')
+  console.log(tbody)
+
+  console.log(carsData)
+
+  carsData.map(function (el) {
+    for (let i = 0; i < tbody.children.length; i++) {
+      let tr = tbody.children[i]
+      let td = [];
+
+      for (let _td = 0; _td < tr.children.length; _td++) {
+        if (tr.children[_td].id.length > 0) td.push(tr.children[_td])
+      }
+
+      console.log(td)
+
+      for (let i = 0; i < td.length; i++) {
+        console.log("compare_image_" + i)
+        if (td[i].id === ("compare_image_" + i)) {
+          const img = document.createElement('img')
+          img.src = carsData[i].image
+          td[i].appendChild(img)
         }
+      }
     }
+  })
 }
